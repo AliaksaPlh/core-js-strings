8de19a12bb669189420e6d18cff6ecb2ff37e292
Aliaksa <sashvero@gmail.com>
2023-12-25 13:11:38 +0300

    feat: 2 first tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -19,8 +19,14 @@
  *   getStringLength(null) => 0
  *   getStringLength(undefined) => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(value) {
+  if (typeof value === 'string') {
+    return value.length;
+  }
+  if (typeof value === 'number') {
+    return value.toString.length;
+  }
+  return 0;
 }
 
 /**
@@ -37,8 +43,8 @@ function getStringLength(/* value */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return typeof value === 'string' || value instanceof String;
 }
 
 /**
